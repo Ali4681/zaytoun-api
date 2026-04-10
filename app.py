aeb263bc6aae601576a8dfe4ba09a10e77c87ec2
ali4681 <hasan.zarangy@gmail.com>
2026-04-11 01:39:41 +0300

    .

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 import numpy as np
-import tflite_runtime.interpreter as tflite
+import tensorflow as tf
 from PIL import Image
 import io
 import base64
@@ -16,7 +16,7 @@ MODEL_PATH     = "olive_model.tflite"
 CLASS_MAP_PATH = "class_mapping.json"
 IMG_SIZE       = 224
 
-interpreter = tflite.Interpreter(model_path=MODEL_PATH)
+interpreter = tf.lite.Interpreter(model_path=MODEL_PATH)
 interpreter.allocate_tensors()
 input_details  = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
